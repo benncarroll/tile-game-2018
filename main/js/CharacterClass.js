377a2da0cef3afabed050af0110efa3bda1d2809
Marcus Karozis <marcus.karozis2@gmail.com>
2018-07-28 13:07:54 +1000

    Revert "Get/Set Functions"
    This reverts commit 68a66ed7b48034a195bf8de69205d443d9e8dc97.

--- main/js/CharacterClass.js
+++ main/js/CharacterClass.js
@@ -12,43 +12,6 @@ Class Character
         this.statEnd = statEnd;
         this.statInt = statInt;
         this.statLck = statLck;
-    }
-    get Name()
-    {
-        return this.name;
-    }
 
-    get Lvl()
-    {
-        return this.lvl;
     }
-    set Lvl(value)
-    {
-        this.lvl = value;
-    }
-
-    get Health()
-    {
-        return this.health;
-    }
-    set Health(value)
-    {
-        this.health = value;
-    }
-
-    get Stats()
-    {
-        return this.statAtk, this.statAgi, this.statCha, this.statEnd, this.statInt, this.statLck;
-    }
-    set Stats(statStr, statAgi, statCha, statEnd, statInt, statLck)
-    {
-        this.statAtk = statStr;
-        this.statAgi = statAgi;
-        this.statCha = statCha;
-        this.statEnd = statEnd;
-        this.statInt = statInt;
-        this.statLck = statLck;
-    }
-
-
 }
