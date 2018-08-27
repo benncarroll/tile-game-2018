4a1343b0c36bec951d419a62cf3d7cdb47797e9c
Marcus Karozis <marcus.karozis2@gmail.com>
2018-08-27 12:53:15 +1000

    combat.js
    - created combat.js file
    - added most of the hollow functions to combat.js

---
+++ phaser/js/combat.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+function combatInit(playerName, tile)
+{
+    while(true)
+    {
+        if (checkEnd())
+        {
+            break;
+        }
+    }
+
+    end();
+}
+
+function checkEnd()
+{
+
+}
+
+function playerTurn()
+{
+
+}
+
+function enemyTurn()
+{
+
+}
+
+function end()
+{
+
+}
