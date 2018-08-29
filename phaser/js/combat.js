a9011a9f323ffec86d9799dfac46b75f5b8c66d7
Marcus Karozis <marcus.karozis2@gmail.com>
2018-08-29 12:53:15 +1000

    importing character data
    - started to import player data
    - started to import enemy data
    - added some commenting

--- phaser/js/combat.js
+++ phaser/js/combat.js
@@ -1,5 +1,17 @@
 /*jshint esversion: 6 */
-function combat(playerName, tile)
+
+/*
+  ,ad8888ba,                                    88
+ d8"'    `"8b                                   88                         ,d
+d8'                                             88                         88
+88              ,adPPYba,   88,dPYba,,adPYba,   88,dPPYba,   ,adPPYYba,  MM88MMM
+88             a8"     "8a  88P'   "88"    "8a  88P'    "8a  ""     `Y8    88
+Y8,            8b       d8  88      88      88  88       d8  ,adPPPPP88    88
+ Y8a.    .a8P  "8a,   ,a8"  88      88      88  88b,   ,a8"  88,    ,88    88,
+  `"Y8888Y"'    `"YbbdP"'   88      88      88  8Y"Ybbd8"'   `"8bbdP"Y8    "Y888
+*/
+
+function combat(tile)
 {
     // initialisation of the combat
     combatInit();
@@ -11,7 +23,7 @@ function combat(playerName, tile)
     while(loop)
     {
         // start enemy turn
-        switch (enemyTurn())
+        switch (enemyTurn(tile))
         {
             case -1:
                 // case for if the player wins
@@ -27,7 +39,7 @@ function combat(playerName, tile)
         }
 
         // start player turn
-        switch (playerTurn())
+        switch (playerTurn(tile))
         {
             case -1:
                 // case for if the player wins
@@ -46,25 +58,68 @@ function combat(playerName, tile)
     end();
 }
 
+/*
+88               88
+88               ""    ,d
+88                     88
+88  8b,dPPYba,   88  MM88MMM
+88  88P'   `"8a  88    88
+88  88       88  88    88
+88  88       88  88    88,
+88  88       88  88    "Y888
+*/
+
 function combatInit()
 {
 
 }
 
-function playerTurn()
+/*
+888888888888
+     88
+     88
+     88  88       88  8b,dPPYba,  8b,dPPYba,   ,adPPYba,
+     88  88       88  88P'   "Y8  88P'   `"8a  I8[    ""
+     88  88       88  88          88       88   `"Y8ba,
+     88  "8a,   ,a88  88          88       88  aa    ]8I
+     88   `"YbbdP'Y8  88          88       88  `"YbbdP"'
+*/
+//(*)
+function playerTurn(tile)
 {
+    var playerHealth = ;
+    var playerStats = ;
+    var playerWpn = ;
+    var playerArmr = ;
+
 
 }
 
-function enemyTurn()
+function enemyTurn(tile)
 {
-
+    var enemyHealth = ;
+    var enemyStats = ;
+    var enemyWpn = ;
+    var enemyArmr = ;
 }
 
 function calcDamage()
 {
 
 }
+
+//(**)
+
+/*
+88888888888                        88
+88                                 88
+88                                 88
+88aaaaa      8b,dPPYba,    ,adPPYb,88
+88"""""      88P'   `"8a  a8"    `Y88
+88           88       88  8b       88
+88           88       88  "8a,   ,d88
+88888888888  88       88   `"8bbdP"Y8
+*/
 
 function end()
 {
